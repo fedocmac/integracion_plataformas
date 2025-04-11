547407dde3d8b6d6edbeeb0218541c91af5db61c
fedocmac <fe.docmac@duocuc.cl>
2025-04-11 19:35:16 -0400

    commit de prueba

--- web/views.py
+++ web/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hola desde la vista index de la app webaa!")
+    return HttpResponse("Hola desde la vista index de la app web.")
