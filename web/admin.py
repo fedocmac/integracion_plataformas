0d26bcce77b267c0dc9588b02f6b0bc135a23ad5
fedocmac <fe.docmac@duocuc.cl>
2025-05-05 00:50:18 -0400

    nuevas paginas
    compra de productos
    historial de productos
    registro de compras en DB

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Categoria, Producto  # Importa tu modelo
+from .models import Categoria, Producto, Compra  # Importa tu modelo
 
 admin.site.register(Categoria)  # Registra el modelo para que sea visible
 admin.site.register(Producto)  # Registra el modelo para que sea visible
+admin.site.register(Compra)
